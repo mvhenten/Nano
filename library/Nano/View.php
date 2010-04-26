0a95ef2c3e7b3a3c9c56c55ce3946414ed8f30ae
matthijs.vanHenten <matthijs.vanHenten@f27d9930-b0da-11de-a90c-f79689ea6043>
2010-04-26 20:54:36 +0000

    added view stub
    git-svn-id: http://mvh-source.googlecode.com/svn/trunk/Nano@134 f27d9930-b0da-11de-a90c-f79689ea6043

---
+++ library/Nano/View.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * basic attempt at a view abstraction.
+ * This implementation is far from generic enough but serves the purpose of
+ * being a placeholder for now!
+ */
+class Nano_View{
+    protected $_base;
+    protected $_path;
+    protected $_layout;
+    protected $_structure;
+    protected $_request;
+    protected $_view;
+    protected $_content;
+
+    public function __construct( $config, Nano_Request $request ){
+        $config = (array) $config;
+        $config['request'] = $request;
+
+        $this->setLayout( $config );    }
+
+    public function setLayout( array $layout ){
+        foreach( $layout as $name => $value ){
+            if( ($property = '_' . $name) && property_exists( $this, $property ) ){
+                $this->$property = $value;
+            }
+        }
+    }
+
+    public function __set( $name, $value ){
+        $this->getContent()->$name = $value;
+    }
+
+    public function __get( $name ){
+        return $this->getContent()->$name;
+    }
+
+    public function __toString(){
+        $layout = sprintf('%s/%s/%s', $this->_base, $this->_path, $this->_layout );
+
+        $view = str_replace( array(':controller', ':action')
+            , array( $this->getRequest()->controller, $this->getRequest()->action )
+            , $this->_structure
+        );
+
+        ob_start();
+        include( sprintf( '%s/%s%s', $this->_base, $this->_path, $view ) );
+        $this->_view = ob_get_clean();
+
+        ob_start();
+        include( $layout );
+
+        return ob_get_clean();
+    }
+
+    private function getContent(){
+        if( null == $this->_content ){
+            $this->_content = new Nano_Collection();
+        }
+        return $this->_content;
+    }
+
+    private function getView(){
+        return $this->_view;
+    }
+
+    public function getRequest(){
+
+        return $this->_request;
+    }
+}
