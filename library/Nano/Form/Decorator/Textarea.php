092ec5788ec598c110128fd5c0372f50a07b4423
matthijs.vanHenten <matthijs.vanHenten@f27d9930-b0da-11de-a90c-f79689ea6043>
2010-10-08 19:59:24 +0000

    bugfix texgtarea
    git-svn-id: http://mvh-source.googlecode.com/svn/trunk/Nano@297 f27d9930-b0da-11de-a90c-f79689ea6043

--- library/Nano/Form/Decorator/Textarea.php
+++ library/Nano/Form/Decorator/Textarea.php
@@ -5,7 +5,14 @@ class Nano_Form_Decorator_Textarea extends Nano_Form_Decorator_Abstract{
         $element->removeAttribute( 'value' );
         $element->removeAttribute( 'type' );
         $element->setVertile(true);
+
+        // return parent::render( $element );
+        $str = parent::render( $element );
         
-        return parent::render( $element );
+        //@todo this fixes something that shouldn'be needin fixin.
+        // whitespace is added to content and this is bad.
+        preg_match( '/^(.+<textarea.+?>)(.+?)(<\/textarea.+)/s', $str, $match );
+        list( $none, $a, $b, $c ) = $match;        
+        return join( "", array($a, trim(htmlentities($b)), $c));        
     }
 }
