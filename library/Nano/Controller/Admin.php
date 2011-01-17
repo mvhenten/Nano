4fd3ab6ef60d1fc923d96a5baf653c6961a01357
matthijs.vanHenten <matthijs.vanHenten@f27d9930-b0da-11de-a90c-f79689ea6043>
2011-01-17 00:28:52 +0000

    cruft
    git-svn-id: http://mvh-source.googlecode.com/svn/trunk/Nano@360 f27d9930-b0da-11de-a90c-f79689ea6043

--- library/Nano/Controller/Admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Nano_Controller_Admin extends Nano_Controller{
-    protected function init(){
-        if( !Nano_Admin::hasIdentity() ){
-            $this->redirect( '/' );
-        }
-    }
-}
