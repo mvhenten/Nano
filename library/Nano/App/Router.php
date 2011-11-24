d0db89160e5ace52a84163180f1a6e582f7f5cd6
Matthijs van Henten <matthijs@ischen.nl>
2011-11-25 00:21:05 +0100

    refactored router into something simple

--- library/Nano/App/Router.php
+++ library/Nano/App/Router.php
@@ -1,65 +1,80 @@
 <?php
 /**
- * @todo remove Nano_Colleciton dependency!
+ * @file Nano/App/Router.php
+ *
+ * A simple request router that matches a series of regexes agains uri patterns
+ * and returns the results of the first match.
+ *
+ * Copyright (C) <2011>  <Matthijs van Henten>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @category   Nano
+ * @package    Nano_Log
+ * @copyright  Copyright (c) 2011 Ischen (http://ischen.nl)
+ * @license    GPL v3
  */
-class Nano_App_Router extends Nano_Collection{
+/**
+ * @class Nano_App_Router
+ * Routing pattenrs can be defined by a subset of regexes.
+ * Currently only \w and \d are supported (whitelisted)
+ *
+ * <code>
+ * my $router = new Nano_App_Router(array(
+ *      '/url/pattern/to' => 'Handler_One',
+ *      '/url/\w+/\d+/?'  => 'Handler_Tow',
+ * ));
+ *
+ * $router->addRoute( '/foo/bar/\d+/', 'Handler_Class' );
+ *
+ * list( $handler, $matches, $pattern ) = $router->getRoute( $url );
+ * </code>
+ */
+class Nano_App_Router {
+
+    private $_routes = array();
+    private $_whitelist = array(
+        '\w' => '\\\w', '\d' => '\\\d',
+        '+'  => '\+', '?'  => '\?',
+        '('  => '\(',  ')'  => '\)',
+        '.' => '\\.'
+    );
+
     public function __construct( $routes ){
-        parent::__construct( (array) $this->getRoute( $routes ) );
-    }
-
-    public function getRequestUri(){
-        return $_SERVER['REQUEST_URI'];
-    }
-
-    private function getRequestValues( $replace ){
-        $url = $this->getRequestUri();
-        $url = str_replace( $replace, '', $url );
-        $url = explode('?', $url );
-        return array_filter(explode( '/', array_shift( $url )));
-    }
-
-    function matchRoute( $uri, $route ){
-        $uri = addslashes(trim( $uri, '/' ));
-
-        //[a-z0-9+\$_-]+(\.[a-z0-9+\$_-]+)*
-        $pattern = preg_replace('/:\w+/', '([A-z0-9+\$_-]+)', $route['route']);
-        preg_match_all('/:(\w+)/', $route['route'], $keys );
-
-        $keys = isset($keys[1])?$keys[1]:array();
-        $pattern = explode('/', trim($pattern, '/'));
-
-        // each uri component is optional until matched
-
-        do{
-            preg_match_all( '/^'.join('\/', $pattern).'/', $uri, $matches, PREG_SET_ORDER );
-
-            if( ! empty($matches[0] ) ){
-                array_shift($matches[0]);
-                if( !empty($matches[0]) && count($keys) == count($matches[0]) ){
-                    $route['defaults'] = array_combine( $keys, $matches[0] ) + $route['defaults'];
-                }
-                return $route['defaults'];
-            }
-
-            array_pop($pattern);
-            array_pop($keys);
-
+        foreach( $routes as $key => $value ){
+            $this->addRoute( $key, $value );
         }
-        while($pattern);
     }
 
-    private function getRoute( $routes ){
-        $url = $this->getRequestUri();
-        $match = array();
+    public function addRoute( $pattern, $handler ){
+        $pattern = preg_quote($pattern, '/');
+        $this->_routes[$pattern] = $handler;
+    }
 
-        foreach( $routes as $route ){
-            $match = $this->matchRoute( $url, (array) $route );
+    public function getRoute( $url ){
+        foreach( $this->_routes as $pattern => $handler ){
+            $pattern = str_replace(
+                array_values( $this->_whitelist ),
+                array_keys( $this->_whitelist ),
+                $pattern
+            );
 
-            if( null !== $match ){
-                return $match;
+            if( preg_match( "/^$pattern$/", $url, $matches ) ){
+                $match = array_shift($matches);
+                return array( $handler, $matches, $match );
             }
         }
-
-        return $route['defaults'];
     }
 }
