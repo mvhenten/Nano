b1996f3e63914e52e9ade9db11d5d520bd8587e3
Matthijs van Henten <matthijs@ischen.nl>
2011-12-07 00:26:23 +0100

    tidied and updated doc

--- library/Nano/Url.php
+++ library/Nano/Url.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * Simple URL object containing the base logic for url manipulation
+ * Simple URL object containing the base logic for url manipulation -
+ * wrapping parse_url, parse_str and http_build_query
  *
  * @file Nano/Url.php
  *
@@ -19,70 +20,89 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *
- * @category   Nano
  * @copyright  Copyright (c) 2011 Ischen (http://ischen.nl)
  * @license    GPL v3
- * @package    Nano
+ * @package    Nano_Url
  */
 
 
 class Nano_Url {
 	private $_scheme = 'http';
-    private $_path = '';
+	private $_path = '';
 	private $_query=array();
 	private $_fragment;
-    private $_user;
-    private $_password;
+	private $_user;
+	private $_password;
 	private $_host;
 	private $_port;
 
 	/**
 	 * Create the url as string
+	 *
 	 * @return string URL
 	 */
 	public function __toString() {
-        return $this->_buildUrl();
+		return $this->_buildUrl();
 	}
 
+
 	/**
-     * Class constructor
+	 * Class constructor
 	 *
-	 * @param string $url Url string that can be parsed trough parse_url
+	 * @param string  $url Url string that can be parsed trough parse_url
 	 */
 	public function __construct( $url ) {
 		$this->parseUrl( $url );
 	}
 
-    public function __get( $name ){
-        if( property_exists( $this, "_$name") && method_exists( $this, $name ) ){
-            return $this->$name();
-        }
-    }
-
-    public function __set( $name, $value ){
-        if( property_exists( $this, "_$name") && method_exists( $this, $name ) ){
-            return $this->$name( $value );
-        }
-    }
 
 	/**
+	 * Magic getter - syntactic sugar for accessing public accessors
+	 * as properties
 	 *
-	 *
-	 * @param unknown $url
+	 * @param unknown $name
+	 * @return unknown Value if exists, or null
 	 */
-	private function parseUrl( $url ) {
-        foreach(  parse_url( $url ) as $method => $value ){
-            $this->$method( $value );
-        }
+	public function __get( $name ) {
+		if ( property_exists( $this, "_$name") && method_exists( $this, $name ) ) {
+			return $this->$name();
+		}
 	}
 
 
 	/**
-     * Returns the scheme, sets the scheme if a valid string is provided
+	 * Magic setter - syntactic sugar to access public accessors as properties
 	 *
-	 * @param unknown $scheme (optional)
-	 * @return unknown
+	 * @param unknown $name
+	 * @param unknown $value
+	 */
+	public function __set( $name, $value ) {
+		if ( property_exists( $this, "_$name") && method_exists( $this, $name ) ) {
+			$this->$name( $value );
+		}
+	}
+
+
+	/**
+	 * Parse url and set corresponding properties of this class
+	 *
+	 * @param string $url
+	 * @return Nano_Url $this This instance
+	 */
+	private function parseUrl( $url ) {
+		foreach (  parse_url( $url ) as $method => $value ) {
+			$this->$method( $value );
+		}
+
+        return $this;
+	}
+
+
+	/**
+	 * Returns the scheme, sets the scheme if a valid string is provided
+	 *
+	 * @param string $scheme (optional) - either 'http' or 'https'
+	 * @return $scheme Current scheme
 	 */
 	public function scheme( $scheme=null ) {
 		if ( is_string($scheme) && preg_match( '{^https?$}', $scheme ) ) {
@@ -94,14 +114,14 @@ class Nano_Url {
 
 
 	/**
+	 * Get/sets the path component
 	 *
-	 *
-	 * @param unknown $path (optional)
-	 * @return unknown
+	 * @param string $path (optional)
+	 * @return string $path Current path
 	 */
 	public function path( $path=null ) {
 		if ( null !== $path && is_string( $path ) ) {
-            $this->_path = trim( $path, '/' );
+			$this->_path = trim( $path, '/' );
 		}
 
 		return '/' . $this->_path;
@@ -109,55 +129,64 @@ class Nano_Url {
 
 
 	/**
+     * Get/sets the path segments as separarate pieces (pathparts)
 	 *
-	 *
-	 * @param unknown $parts (optional)
-	 * @return unknown
+	 * @param array $parts   (optional) Optional new pathparts
+	 * @param int $padding (optional) Padds the output, if needed. Handy to use with list(...)
+	 * @return array $path_parts Path segments of the current path
 	 */
 	public function pathParts( $parts=null, $padding=0 ) {
 		if ( is_array($parts) ) {
 			$this->_path = join( '/', array_filter($parts) );
 		}
 
-        $path_parts = explode( '/', trim($this->_path, '/') );
+		$path_parts = explode( '/', trim($this->_path, '/') );
 
 		if ( is_int($padding) && $padding > 0 ) {
 			$path_parts = array_pad( $path_parts, $padding, null );
 		}
 
-        return $path_parts;
+		return $path_parts;
 	}
 
 
 	/**
+	 * Get/sets the query component as string
 	 *
-	 *
-	 * @param unknown $query (optional)
-	 * @return unknown
+	 * @param string $query (optional) New query component
+	 * @return string $query Current query component
 	 */
 	public function query( $query='' ) {
 		if ( is_string( $query ) && !empty($query) ) {
-            parse_str( $query, $query_hash );
+			parse_str( $query, $query_hash );
 			$this->_query = $query_hash;
 		}
 
 		return http_build_query( $this->_query );
 	}
 
-    public function query_form( array $query_form = array() ){
-        if( count($query_form) > 0 ){
-            $this->_query = $query_form;
-        }
 
-        return $this->_query;
-    }
+	/**
+     * Get/Sets the query component as an array
+	 * @see parse_str
+	 *
+	 * @param array   $query_form (optional) Optional new query parameters
+	 * @return array $query_params Query parameters as array
+	 */
+	public function query_form( array $query_form = array() ) {
+		if ( count($query_form) > 0 ) {
+			$this->_query = $query_form;
+		}
+
+		return $this->_query;
+	}
 
 
 	/**
+	 * Get/sets the url fragment component
 	 *
-	 *
-	 * @param unknown $fragment (optional)
-	 * @return unknown
+	 * @param string $fragment (optional)
+	 * @return string $fragment
 	 */
 	public function fragment( $fragment=null ) {
 		if ( is_string( $fragment ) ) {
@@ -169,13 +198,13 @@ class Nano_Url {
 
 
 	/**
+	 * Get/Sets hostname component
 	 *
-	 *
-	 * @param unknown $hostname (optional)
-	 * @return unknown
+	 * @param string $hostname (optional) New hostname
+	 * @return string $hostname
 	 */
 	public function host( $hostname='' ) {
-		if ( ! empty( $hostname ) ) {
+		if ( ! empty( $hostname ) && is_string($hostname) ) {
 			$this->_host = $hostname;
 		}
 
@@ -184,7 +213,7 @@ class Nano_Url {
 
 
 	/**
-	 *
+     * Get/Sets port number component
 	 *
 	 * @param unknown $portnum (optional)
 	 * @return unknown
@@ -197,37 +226,67 @@ class Nano_Url {
 		return $this->_port;
 	}
 
-    public function user( $user = null ){
-        if( null !== $user ){
-            $this->_user = $user;
-        }
 
-        return $this->_user;
-    }
+	/**
+     * Get/Sets the user component - used in basic auth
+	 *
+	 * @param string $user (optional)
+	 * @return string $username
+	 */
+	public function user( $user = null ) {
+		if ( null !== $user && is_string($user) ) {
+			$this->_user = $user;
+		}
 
-    public function pass( $password = null ){
-        return $this->password( $password );
-    }
+		return $this->_user;
+	}
 
-    public function password( $password = null ){
-        if( null !== $password ){
-            $this->_password = $password;
-        }
+	/**
+     * Get/Sets pass component - alias for password to maintain symetry to parse_url
+     * @see Nano_Url::password
+	 *
+	 * @param string $password (optional)
+	 * @return string $password
+	 */
+	public function pass( $password = null ) {
+		return $this->password( $password );
+	}
 
-        return $this->_password;
-    }
 
-    private function _buildUrl(){
-        return join( '', array(
-            $this->scheme() . '://',
-            join( '@', array(
-                join( ':', array( $this->user, $this->password )),
-                join( ':', array( $this->host, $this->port))
-            )),
-            join( '?', array(
-                $this->path(),
-                join( '#', array( $this->query, $this->fragment )),
-            )),
-        ));
-    }
+	/**
+     * Get/Sets password component used in basic auth
+     * @see Nano_Url::password
+	 *
+	 * @param string $password (optional)
+	 * @return string $password
+	 */
+	public function password( $password = null ) {
+		if ( null !== $password ) {
+			$this->_password = $password;
+		}
+
+		return $this->_password;
+	}
+
+
+	/**
+	 * Builds new url from components
+	 *
+	 * @return string $url
+	 */
+	private function _buildUrl() {
+		return join( '', array(
+				$this->scheme() . '://',
+				join( '@', array(
+						join( ':', array( $this->user, $this->password )),
+						join( ':', array( $this->host, $this->port))
+					)),
+				join( '?', array(
+						$this->path(),
+						join( '#', array( $this->query, $this->fragment )),
+					)),
+			));
+	}
+
+
 }
