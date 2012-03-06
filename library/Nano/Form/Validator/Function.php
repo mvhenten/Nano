f127ccfe50b529b500a530492dd5264a6cd9d42d
Matthijs van Henten <matthijs@ischen.nl>
2012-03-07 00:24:48 +0100

    fixed function validator

--- library/Nano/Form/Validator/Function.php
+++ library/Nano/Form/Validator/Function.php
@@ -9,11 +9,14 @@
  *
  * PHP version 5
  *
- * @author     Mathijs van Henten <matthijs@ischen.nl>
  * @copyright  2009 Matthijs van Henten
  * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @author     Mathijs van Henten <matthijs@ischen.nl>
+ * @package Nano
  */
-class Nano_Form_Validation{
+
+
+class Nano_Form_Validator_Function {
     private $method;
     private $arguments;
     private $message;
@@ -21,39 +24,42 @@ class Nano_Form_Validation{
     /**
      * Create a new Form_Validation
      *
-     * @param mixed $method Either input valid for call_user_func or create_function
-     * @param array $arguments Key=>value pairs of additional arguments
-     * @param string $message (optional) Error message to associate
      *
      * @return Nano_Form_Validation $validation
+     * @param mixed   $method    Either input valid for call_user_func or create_function
+     * @param array   $arguments (optional) Key=>value pairs of additional arguments
+     * @param string  $message   (optional) Error message to associate
      */
-    public function __construct( $method, $arguments = null, $message = null ){
+    public function __construct( $method, $arguments = null, $message = null ) {
         $this->method       = $method;
         $this->arguments    = $arguments;
         $this->message      = $message;
     }
 
+
     /**
      * Execute the validation function
      * -- compiles and runs the function passed as a constructor
      *
-     * @param mixed $value Value to validate. This is injected as the first
      *                      argument of the validation function
      *
+     * @param mixed   $value Value to validate. This is injected as the first
      * @return bool $valid
      */
-    public function execute( $value ){
+    public function validate( $value ) {
         $args = array_values( $this->getArguments() );
         // assert the order of parameters!
-        if( count( $args ) > 0 ){
+        if ( count( $args ) > 0 ) {
             array_unshift( $args, $value );
         }
-        else{
+        else {
             $args = (array) $value;
         }
 
         return (bool) call_user_func_array( $this->getMethod(), $args );
+
     }
+
 
     /**
      * Return method for validation
@@ -61,13 +67,14 @@ class Nano_Form_Validation{
      *
      * @return function $function Function parameter suitable for call_user_func
      */
-    private function getMethod(){
-        if( ! is_callable( $this->method ) ){
+    private function getMethod() {
+        if ( ! is_callable( $this->method ) ) {
             $this->method = $this->createFunction();
         }
 
         return $this->method;
     }
+
 
     /**
      * Create an anonymous function. takes care of creating the variable names
@@ -75,34 +82,36 @@ class Nano_Form_Validation{
      *
      * @return function $function Method for validation
      */
-    private function createFunction(){
+    private function createFunction() {
         $args = $this->getArguments();
         $key  = 'value';
-        if( count( $args ) > 0 ){
+        if ( count( $args ) > 0 ) {
             $keys = array_keys( $args );
             array_unshift( $keys, $key );
         }
-        else{
+        else {
             $keys = (array) $key;
         }
 
-        foreach( $keys as $index => $key ){
+        foreach ( $keys as $index => $key ) {
             $keys[$index] = '$' . $key;
         }
 
         return create_function( join( ',', $keys ), $this->method );
     }
 
+
     /**
      * Return additional arguments passed to the constructor
      *
      * @return array $arguments
      */
-    private function getArguments(){
-        if( null == $this->arguments ){
+    private function getArguments() {
+        if ( null == $this->arguments ) {
             $this->arguments = array();
         }
         return $this->arguments;
     }
+
+
 }
-?>
